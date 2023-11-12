1cd148f5bbb690f03d0c1d42c690ecfd083508b3
jennmyoon <jenniferminahyoon@gmail.com>
2023-11-11 18:26:49 -0800

    updated code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import infoButton from './Assests/info-Button.svg';
 import Home from './pages/Home';
 import Gallery from './pages/Gallery';
 import Navbar from './Components/Navbar';
 import {ModalProvider} from './Components/ModalContext.js'; 
+import { useEffect } from 'react';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Redirect to the home route when the component mounts
+    navigate('/');
+  }, [navigate]);
+
   return (
     <div id='app-wrapper'>
       <ModalProvider>
